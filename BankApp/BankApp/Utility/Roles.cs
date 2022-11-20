4d57706a3384bb6470d7ef9618ddbd7e779fca91
SlushiestTurtle <tony_philipsen_@live.se>
2022-11-20 20:04:16 +0100

    update
    update

--- BankApp/BankApp/Utility/Roles.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BankApp.Utility
-{
-    public static class Roles
-    {
-        public const string Admin = "admin";
-        public const string BankManager = "bankManager";
-        public const string AppUser = "appUser";
-    }
-}
